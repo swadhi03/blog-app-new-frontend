fa551c9f4f535ea8009527b6bd4931781ce907e1
swadhi03 <swathiksanthosh03@gmail.com>
2024-07-26 00:37:41 +0530

    completed

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,21 +2,28 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 const SignUp = () => {
-    const [data,changeData]=useState([])
+    const [data,changeData]=useState(
+        {"name":"", "email":"", "password":"", "cfmpassword":""}
+    )
     const inputHandler=(event)=>{
         changeData({...data,[event.target.name]:event.target.value})
     }
     const readValue=()=>{
-        console.log(data)
-        axios.post("http://localhost:8080/signup",data).then(
+        if (data.password==data.cfmpassword) {
+            let newInput={"name":data.name, "email":data.email, "password":data.password}
+
+
+        axios.post("http://localhost:8080/signup",newInput).then(
             (response)=>{
                 console.log(response.data)
                 if(response.data.status=="success")
                     {
-                        alert("success")
+                        alert("Registered Successfully")
+                        changeData({"name":"", "email":"", "password":"", "cfmpassword":""})
                     }
                     else{
-                        alert("failed")
+                        alert("Registration failed")
+                        changeData({"name":"", "email":"", "password":"", "cfmpassword":""})
                     }
             }
         ).catch(
@@ -25,6 +32,9 @@ const SignUp = () => {
                 alert(error.message)
             }
         ).finally()
+        } else {
+            alert("Password and Confirm password doesn't match")
+        }
     }
   return (
     <div>
@@ -45,10 +55,17 @@ const SignUp = () => {
                             <input type="password" className="form-control" name='password' value={data.password} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Confirm Password</label>
+                            <input type="password" className="form-control" name='cfmpassword' value={data.cfmpassword} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <button className="btn btn-success" onClick={readValue}>Sign Up</button>
                         </div>
+                        <div>
+                            <a href="/signin" className='btn btn-primary'>BackTo Login</a>
+                        </div>
                     </div>
-                    <a href="/signin">Existing user Click Here</a>
+        
                 </div>
             </div>
         </div>
