81ee159f2603affe46dbfdca3205218dc7f7b7dc
swadhi03 <swathiksanthosh03@gmail.com>
2024-07-28 13:20:13 +0530

    completed

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -25,9 +25,7 @@ const ViewAll = () => {
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
                         <div className="row g-3">
-
                             {data.map(
                                 (value, index) => {
                                     return <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -45,9 +43,7 @@ const ViewAll = () => {
                                             </div>
                                         </div>
                                     </div>
-
                                 </div>
-
                                 }
                             )}
                         </div>
