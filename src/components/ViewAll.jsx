22baac5583171c2b90aedcd2b9da9b856fcc71fc
swadhi03 <swathiksanthosh03@gmail.com>
2024-07-28 13:18:01 +0530

    View all post completed

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -34,7 +34,7 @@ const ViewAll = () => {
                                     <div class="card mb-3">
                                         <div class="row g-0">
                                             <div class="col-md-4">
-                                                <img src="..." class="img-fluid rounded-start" alt="..." />
+                                                <img src={value.image} class="img-fluid rounded-start" alt="..." />
                                             </div>
                                             <div class="col-md-8">
                                                 <div class="card-body">
